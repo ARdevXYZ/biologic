ec7800c554c688308db3e515f1683a9e0e928d2c
Andrew Rosinski <andrew.rosinski@gmail.com>
2020-07-22 14:47:16 -0400

    Bio Logic
    Initial push/commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main role="main" className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="Biologic logo" title="Biologic Logo" />
+        <section className="text-container">
+          <p>We are Biologic. <i className="fas fa-vial"></i><br /> We ♥︎ process efficiencies.<br /> Let us optimize your growth within the cannabis industry.
+          </p>
+          <p>
+            <a
+                className="app-link"
+                title="Contact Us"
+                alt="Contact Us"
+                href="mailto:info@biologic.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                info@biologic.consulting
+            </a>
+          </p>
+
+        </section>
       </header>
-    </div>
+    </main>
   );
 }
 
